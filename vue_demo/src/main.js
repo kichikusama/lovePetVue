8be0dc85000b4178babd6c7bf978d6feae59c413
gouminm <975011091@qq.com>
2019-05-06 15:32:06 +0800

    提交

--- vue_demo/src/main.js
+++ vue_demo/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+// import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
@@ -11,6 +11,6 @@ Vue.use(ElementUI);
 
 new Vue({
   router, //配置路由，在Vue实例中添加 路由对象
-  store, // 状态机
+  // store, // 状态机
   render: h => h(App),
 }).$mount('#app')
--- vue_demo/src/store.js
+++ vue_demo/src/store.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import counter from './store/counter';
-import todolist from './store/todolist';
-import students from './store/students.js';
-import messages from './store/messages.js';
+// import counter from './store/counter';
+// import todolist from './store/todolist';
+// import students from './store/students.js';
+// import messages from './store/messages.js';
 
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules:{
-      counter,
-      todolist,
-      students,
-      messages,
+      // counter,
+      // todolist,
+      // students,
+      // messages,
   }
 })
