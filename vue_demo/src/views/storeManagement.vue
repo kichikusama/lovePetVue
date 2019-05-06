182ae0bca86b76a7fd4c98893f219ab0de0aff1e
kichikusama <nickli.ldy@gmail.com>
2019-05-06 16:31:24 +0800

    更改了平台左侧的结构

--- vue_demo/src/views/storeManagement.vue
+++ vue_demo/src/views/storeManagement.vue
@@ -18,17 +18,9 @@
             <span>商品管理</span>
           </template>
           <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="/storeManagement/users">用户</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
+            <el-menu-item index="1-1">新增商品</el-menu-item>
+            <el-menu-item index="1-2">查询商品</el-menu-item>
+            </el-menu-item-group>
         </el-submenu>
 
         <el-submenu index="2">
@@ -39,8 +31,7 @@
           <el-menu-item-group>
             
             <el-menu-item index="1-1">新增服务</el-menu-item>
-            <el-menu-item index="1-2">更改服务</el-menu-item>
-            <el-menu-item index="1-3">查询服务</el-menu-item>
+            <el-menu-item index="1-2">查询服务</el-menu-item>
 
           </el-menu-item-group>
         </el-submenu>
@@ -53,8 +44,7 @@
           <el-menu-item-group>
 
             <el-menu-item index="1-1">新增宠物</el-menu-item>
-            <el-menu-item index="1-2">更改宠物</el-menu-item>
-            <el-menu-item index="1-3">查询宠物</el-menu-item>
+            <el-menu-item index="1-2">查询宠物</el-menu-item>
           </el-menu-item-group>
           
         </el-submenu>
@@ -65,17 +55,8 @@
             <span>订单管理</span>
           </template>
           <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
+            <el-menu-item index="1-1">订单列表</el-menu-item>
+            </el-menu-item-group>
         </el-submenu>
 
       </el-menu>
