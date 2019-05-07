7b28620a911121b1ad48d42eb5020d24e565ea6f
kichikusama <nickli.ldy@gmail.com>
2019-05-07 16:53:40 +0800

    no message

--- vue_demo/src/components/goods/addGoods.vue
+++ vue_demo/src/components/goods/addGoods.vue
@@ -106,7 +106,7 @@
 // <script>
 export default {
    computed: {
-    ...mapState(["goods", "currentPage", "totalPage", "count", "films"])
+    // ...mapState(["goods", "currentPage", "totalPage", "count", "films"])
   },
   methods: {
     onSubmit() {
