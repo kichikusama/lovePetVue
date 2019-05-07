e0a3a761406a0e438bea674d0ee61c18a6c547bc
kichikusama <nickli.ldy@gmail.com>
2019-05-07 15:19:49 +0800

    不知道写了些啥

--- vue_demo/servise/service.js
+++ vue_demo/servise/service.js
@@ -1,7 +1,8 @@
-const addService = async ({} = {}) => await fetch(`/service/addService?`).then(response => response.json());
-const findService = async ({} = {}) => await fetch(`/service/findService?`).then(response => response.json());
+const addService = async ({} = {}) => await fetch(`/service/addService`).then(response => response.json());
+const getService = async ({ currentPage = 1, eachPage = 10  } = {}) => await fetch(`/service/findService?currentPage=${currentPage}&eachPage=${eachPage}`).then(response => response.json());
+const findService = async ({} = {}) => await fetch(`/service/findService`).then(response => response.json());
 export default {
   addService,
-  findService,
-
+  getService,
+  findService
 }
