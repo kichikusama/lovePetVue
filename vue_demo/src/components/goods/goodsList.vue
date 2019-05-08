9f2f5d25b348c4df14144de39329e488b98badb4
chenxinyu <1443356686@qq.com>
2019-05-08 18:04:18 +0800

    新增 商品列表的数据获取

--- vue_demo/src/components/goods/goodsList.vue
+++ vue_demo/src/components/goods/goodsList.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="width:90%;padding:20px">
-    <!-- <el-table :data="tableData" border style="width: 100%">
+    <el-table :data="goods" border style="width: 100%">
       <el-table-column fixed prop="goodsName" label="名称" width="150"></el-table-column>
       <el-table-column prop="goodsType" label="品类" width="120"></el-table-column>
       <el-table-column prop="goodsMaterial" label="材质" width="120"></el-table-column>
@@ -15,30 +15,58 @@
       <el-table-column prop="goodsSupplier" label="供应商" width="120"></el-table-column>
       <el-table-column prop="goodsIntro" label="特色说明" width="300"></el-table-column>
       <el-table-column prop="goodsPrice" label="价格" width="120"></el-table-column>
-      <el-table-column fixed="goodsImg" label="包装" width="100">
+      <el-table-column prop="goodsImg" label="商品图片" width="120">
         <template slot-scope="scope">
-          <el-button @click="handleClick(scope.row)" type="text" size="small">删除</el-button>
-          <el-button type="text" size="small">编辑</el-button>
+          <img style="width:80px;height:80px" :src="scope.row.goodsImg" alt>
         </template>
       </el-table-column>
-    </el-table> -->
-    <!-- <el-pagination
+      <el-table-column fixed="right" label="操作" width="200">
+        <template slot-scope="scope">
+          <el-button size="mini" >编辑</el-button>
+          <el-button size="mini" type="danger" @click="deleteGoodsByPageAsync(scope.row._id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
       @size-change="setEachPage"
       @current-change="setCurrentPage"
       :current-page="currentPage-0"
-      :page-sizes="[2,4,6,8,10]"
+      :page-sizes="[1,2,3,4,5]"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="count"
-    ></el-pagination> -->
+      :total="count-0"
+    ></el-pagination>
   </div>
 </template>
 
 <script>
+import { createNamespacedHelpers } from "vuex";
+const { mapState, mapActions, mapMutations } = createNamespacedHelpers("goodsList");
 export default {
-  methods: {
-    handleClick(row) {
-      console.log(row);
+  watch: {
+    eachPage() {
+      this.getGoodsByPageAsync();
+    },
+    currentPage() {
+      this.getGoodsByPageAsync();
     }
   },
+  computed: {
+    ...mapState(["totalPage","count","goods"]),
+    eachPage: {
+      get:mapState(["eachPage"]).eachPage,
+      set: mapMutations(["setEachPage"]).setEachPage
+    },
+    currentPage: {
+      get: mapState(["currentPage"]).currentPage,
+      set: mapMutations(["setEachPage"]).setCurrentPage
+    }
+  },
+  methods: {
+    ...mapActions(["getGoodsByPageAsync","deleteGoodsByPageAsync"]),
+    ...mapMutations(["setEachPage", "setCurrentPage"]),
+  },
+  mounted() {
+    this.getGoodsByPageAsync();
+  }
 };
 </script>
