2477d30e141df572e6fa9cace339b5f3a09b83f6
chenxinyu <1443356686@qq.com>
2019-05-08 10:44:54 +0800

    完善 新增商品功能

--- pet/service/goodsService.js
+++ pet/service/goodsService.js
@@ -1,12 +1,12 @@
 const { addGoods } = require('../dao/goodsDao.js');
 
 module.exports.addGoods = async function (data) {
-    const result = await addUser(data);
-    // if (result) {
-    //     return true;
-    // } else {
-    //     return false;
-    // }
+    const result = await addGoods(data);
+    if (result) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports.getUsers = async function () {
