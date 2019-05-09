132003671bdbfdf90537955e3f1bc671d8e1c991
chenxinyu <1443356686@qq.com>
2019-05-09 14:46:38 +0800

    新增 商品查询功能

--- vue_demo/servise/goods.js
+++ vue_demo/servise/goods.js
@@ -17,15 +17,7 @@ const getGoodsByPage = async (data) => await fetch(`/goods/getGoodsByPage`,{
 // const getGoodsByPage = async ({ currentPage = 1, eachPage = 5 } = {}) => await fetch(`/goods/getGoodsByPage?currentPage=${currentPage}&eachPage=${eachPage}`).then(response => response.json());
 const deleteGoodsByPage = async (data) => await fetch(`/goods/deleteGoodsByPage?data=${data}`).then(response => response.json());
 
-const searchGoodsByType = async (data) => await fetch(`/goods/searchGoodsByType`,{
-    headers: { 
-        "Content-Type": "application/json"
-    },
-    method:'POST',
-    body:  JSON.stringify(data)
-}).then(response => response.json())
-
 
 export default {
-    addGoods,getGoodsByPage,deleteGoodsByPage,searchGoodsByType
+    addGoods,getGoodsByPage,deleteGoodsByPage
 }
