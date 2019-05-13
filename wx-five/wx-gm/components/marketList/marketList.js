733190656a6b264af217486fcf77928ee32b1331
ig666 <1249731680@qq.com>
2019-05-13 18:25:22 +0800

    微信

---
+++ wx-five/wx-gm/components/marketList/marketList.js
@@ -0,0 +1,23 @@
+// components/marketList/marketList.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    serve:["宠物","服务类型","商品"]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
