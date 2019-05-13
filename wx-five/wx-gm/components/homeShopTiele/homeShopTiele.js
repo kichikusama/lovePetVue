1f856f0a469c3a97fe9b28343b02a6a33268697b
chenxinyu <1443356686@qq.com>
2019-05-13 18:25:03 +0800

    提交部分微信功能

---
+++ wx-five/wx-gm/components/homeShopTiele/homeShopTiele.js
@@ -0,0 +1,26 @@
+// components/homeShopTiele/homeShopTiele.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    shopImg:"../../assets/home/shopTitle4.jpg",
+    positionImg:"../../assets/home/position.png",
+    shopName:"好可爱宠物商店",
+    shopAdd:"成都市顺城大街20号"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
